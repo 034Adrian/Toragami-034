9be2220b7958018eeee18282d9ba5f44d2704ebc
Adrian_034 <adrianmesasacasas@outlook.com>
2022-10-15 11:58:34 -0500

    Uppdt

---
+++ Pages/Productos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesProductos.pages
+{
+    public class ProductosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
